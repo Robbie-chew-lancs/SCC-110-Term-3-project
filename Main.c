c233d36e0d5cf9a36ec76120977cc2cd68215ced
Chew <chewr1@vdi-scc130-027.lancs.local>
2023-04-30 10:43:39 +0100

    re architected

--- Main.c
+++ Main.c
@@ -2,22 +2,35 @@
 #include <string.h>
 
 int main(){
-    char FileAddress1[255];
-    printf("File 1:");
-    scanf("%s", FileAddress1);
+//    char FileAddress1[255];
+//    printf("File 1:");
+//    scanf("%s", FileAddress1);
+//
+//
+//    FILE *textfile;
+//
+//    textfile = fopen(FileAddress1, "r");
+//
+//    char FileAddress2[255];
+//    printf("File 2:");
+//    scanf("%s", FileAddress2);
+//
+//
+//    FILE *textfile;
+//
+//    textfile = fopen(FileAddress2, "r");
+//
 
+    FILE *textFile1;    
+    FILE *textFile2;
+    FILE *textFile3;
+    FILE *textFile4;
+    FILE *textFile5;    
 
-    FILE *textfile;
-
-    textfile = fopen(FileAddress1, "r");
-
-    char FileAddress2[255];
-    printf("File 2:");
-    scanf("%s", FileAddress2);
-
-
-    FILE *textfile;
-
-    textfile = fopen(FileAddress2, "r");
-
+    textFile1 = fopen("./test", "r");
+    textFile2 = fopen("./test", "r");
+    textFile3 = fopen("./test", "r");
+    textFile4 = fopen("./test", "r");
+    textFile5 = fopen("./test", "r");    
+    
 }
