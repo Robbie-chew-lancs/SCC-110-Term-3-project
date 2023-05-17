d7a7ec80904361a59b4f09f177d22aaf147c9e80
Chew <chewr1@vdi-scc130-025.lancs.local>
2023-05-17 07:39:38 +0100

    changed variable names to be consitent

--- Main.c
+++ Main.c
@@ -21,39 +21,39 @@ char *filter(char *mystring)
     return mystring;
 }
 
-struct wordFrequency{
+struct wordfrequency{
     char word[MAX_LEN];
     int frequency;
 };
 
 
-void analyse_file(FILE *textFile){
-    char mystring[101];
+void analyse_file(FILE *textfile){
+    char filedata[101];
     char space[2] = " ";
-    struct wordFrequency wordArray[MAX_WORDS];
-    int numWords = 0; 
+    struct wordfrequency wordarray[MAX_WORDS];
+    int numwords = 0; 
 
-    while (fgets(mystring, 100, textFile)){
+    while (fgets(filedata, 100, textfile)){
         char* token;//individual word tokens
 
-        token = strtok(mystring, space);// splits the string into tokens, spliting on spaces 
+        token = strtok(filedata, space);// splits the string into tokens, spliting on spaces 
 
         while(token != NULL){
             char* filteredtoken = filter(token);//removes punctuation
 
             int found = 0;
-            for(int i=0; i < numWords; i++){
-                if(strcmp(filteredtoken, wordArray[i].word) ==0){
-                    wordArray[i].frequency++;
+            for(int i=0; i < numwords; i++){
+                if(strcmp(filteredtoken, wordarray[i].word) ==0){
+                    wordarray[i].frequency++;
                     found = 1;
                     break;
                 }
             }
             
-            if((found ==0) && (numWords < MAX_WORDS)){
-                strcpy(wordArray[numWords].word, filteredtoken);
-                wordArray[numWords].frequency = 1;
-                numWords++;
+            if((found ==0) && (numwords < MAX_WORDS)){
+                strcpy(wordarray[numwords].word, filteredtoken);
+                wordarray[numwords].frequency = 1;
+                numwords++;
                 //printf("%s\n", filteredtoken);
             }
 
@@ -62,9 +62,9 @@ void analyse_file(FILE *textFile){
         }
     }
 
-    for(int i = 0; i < numWords; i++){
-        printf("%s  ", wordArray[i].word);
-        printf("Count: %i\n", wordArray[i].frequency);
+    for(int i = 0; i < numwords; i++){
+        printf("%s  ", wordarray[i].word);
+        printf("Count: %i\n", wordarray[i].frequency);
 
     }
 
@@ -72,29 +72,29 @@ void analyse_file(FILE *textFile){
 
 int main(){
 
-    FILE *textFile1;    
-    FILE *textFile2;
-    FILE *textFile3;
-    FILE *textFile4;
-    FILE *textFile5;    
+    FILE *textfile1;    
+    FILE *textfile2;
+    FILE *textfile3;
+    FILE *textfile4;
+    FILE *textfile5;    
 
 
     //note that this will only work if the files are in the same folder that the program is executed. aditionaly, the names are fixed 
-    textFile1 = fopen("test1.txt", "r");
-    textFile2 = fopen("test2.txt", "r");
-    textFile3 = fopen("test3.txt", "r");
-    textFile4 = fopen("test4.txt", "r");
-    textFile5 = fopen("test5.txt", "r");    
+    textfile1 = fopen("test1.txt", "r");
+    textfile2 = fopen("test2.txt", "r");
+    textfile3 = fopen("test3.txt", "r");
+    textfile4 = fopen("test4.txt", "r");
+    textfile5 = fopen("test5.txt", "r");    
 
     printf("Analysing file 1....\n");
-    analyse_file(textFile1);    
+    analyse_file(textfile1);    
     printf("Analysing file 2....\n");
-    analyse_file(textFile2);
+    analyse_file(textfile2);
     printf("Analysing file 3....\n");
-    analyse_file(textFile3);
+    analyse_file(textfile3);
     printf("Analysing file 4....\n");
-    analyse_file(textFile4);
+    analyse_file(textfile4);
     printf("Analysing file 5....\n");
-    analyse_file(textFile5);
+    analyse_file(textfile5);
 }
 
